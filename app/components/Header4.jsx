e9d3972df28f6d65346baf4d597f5a66d9342412
Aman Bhandari <73430863+Aman949@users.noreply.github.com>
2024-07-20 21:42:04 +0530

    Update Header4.jsx

--- app/components/Header4.jsx
+++ app/components/Header4.jsx
@@ -10,21 +10,21 @@ const Header4 = () => {
           <div className='grid sm:grid-cols-1 gap-4'>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/planing.svg' height={40} width={40} alt='planning' />
+                <Image src='./planing.svg' height={40} width={40} alt='planning' />
                 <p className='font-bold'>Analysis</p>
               </div>
               <p className='pl-4'>We craft precise technical specs, aligning with your business, technology, and user requirements.</p>
             </div>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/graphic-design.svg' height={40} width={40} alt='graphic' />
+                <Image src='./graphic-design.svg' height={40} width={40} alt='graphic' />
                 <p className='font-bold'>Design</p>
               </div>
               <p className='pl-4'>We adapt the customer&apos;s corporate identity to the platform guidelines. We draw convenient and understandable interfaces.</p>
             </div>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/coding-language.svg' height={40} width={40} alt='coding' />
+                <Image src='./coding-language.svg' height={40} width={40} alt='coding' />
                 <p className='font-bold'>Development</p>
               </div>
               <p className='pl-4'>We create an extensible architecture, write clean and stable code. We integrate with customer technologies.</p>
@@ -32,27 +32,27 @@ const Header4 = () => {
           </div>
         </div>
         <div className='col-span-4'>
-          <Image src='/phn.png' height={400} width={400} alt='phone' className='ml-20' />
+          <Image src='./phn.png' height={400} width={400} alt='phone' className='ml-20' />
         </div>
         <div className='sm:col-span-4'>
           <div className='grid sm:grid-cols-1 gap-4'>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/Security.svg' height={40} width={40} alt='planning' />
+                <Image src='./Security.svg' height={40} width={40} alt='planning' />
                 <p className='font-bold'>Testing</p>
               </div>
               <p className='pl-4'>We carry out functional testing and do bug fixes. We adapt the application to different phone resolutions.</p>
             </div>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/hosting 1.svg' height={40} width={40} alt='planning' />
+                <Image src='./hosting 1.svg' height={40} width={40} alt='planning' />
                 <p className='font-bold'>Launching</p>
               </div>
               <p className='pl-4'>We design the application page and publish it in the App Store and Google Play stores.</p>
             </div>
             <div className='border border-white/80 shadow-xl rounded-lg p-4'>
               <div className='flex items-center p-4 gap-2'>
-                <Image src='/application.svg' height={40} width={40} alt='planning' />
+                <Image src='./application.svg' height={40} width={40} alt='planning' />
                 <p className='font-bold'>Support</p>
               </div>
               <p className='pl-4'>We monitor the stability of the application, update it for new devices and versions of iOS and Android.</p>
