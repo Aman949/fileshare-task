d7dfed5e0c20ecd8d6f63dfffab69139892de1cc
Aman Bhandari <73430863+Aman949@users.noreply.github.com>
2024-07-20 21:43:20 +0530

    Update Nav.jsx

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <nav className='bg-white border border-gray-200 rounded-xl flex flex-wrap items-center p-4 sm:m-10'>
       <div className='flex items-center gap-8 sm:gap-12 md:gap-16 flex-1'>
-        <Image src='/logo-bg-2 1.png' width={150} height={150} alt='logo' />
+        <Image src='./logo-bg-2 1.png' width={150} height={150} alt='logo' />
         <div className='flex gap-6 md:gap-10 sm:mr-4'>
           <Link href='' className='hover:text-blue-500 transition duration-300'>Services</Link>
           <Link href='' className='hover:text-blue-500 transition duration-300'>Media</Link>
@@ -16,8 +16,8 @@ const Nav = () => {
         </div>
       </div>
       <div className='flex gap-4 md:gap-8'>
-        <Image src='/top1.png' width={150} height={150} alt='top1' />
-        <Image src='/top2.png' width={150} height={150} alt='top2' />
+        <Image src='./top1.png' width={150} height={150} alt='top1' />
+        <Image src='./top2.png' width={150} height={150} alt='top2' />
       </div>
     </nav>
   )
