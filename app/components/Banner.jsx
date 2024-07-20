0559e6a2548f598de10cf44217d807771c0714de
Aman Bhandari <73430863+Aman949@users.noreply.github.com>
2024-07-20 08:01:17 -0700

    Update Banner.jsx

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Details from './Details'
+import React from 'react';
+import Details from './Details';
 
 const Banner = () => {
   return (
     <div className='bg-gradient-to-r from-yellow-500 to-green-500 flex justify-around'>
       <div className='p-14'>
-        <h1 className=' text-white font-bold text-4xl'>Lets Discuss
+        <h1 className=' text-white font-bold text-4xl'>Let&apos;s Discuss
           <br />your project
         </h1>
-        <p className=' text-white pt-10 text-xl'>Let's figure out how to create an effective application,<br />
-its cost and terms of its development</p>
+        <p className=' text-white pt-10 text-xl'>Let&apos;s figure out how to create an effective application,<br />
+          its cost and terms of its development
+        </p>
       </div>
       <div>
-        <Details/>
+        <Details />
       </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
